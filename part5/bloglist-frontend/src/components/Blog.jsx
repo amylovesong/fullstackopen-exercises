540649c6cc3efb69249798531a5e75edd6c8d68b
amylovesong <amylovesong.sun@gmail.com>
2025-01-10 14:42:26 +0800

    Add part5 exercise-5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const blogDetails = blog => {
+    return <div>
+      <div>{blog.url}</div>
+      <div>
+        {blog.likes}
+        <button>like</button>
+      </div>
+      <div>{blog.user.name}</div>
+    </div>
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleShowDetails}>{showDetails ? 'hide' : 'view'}</button>
+      {showDetails && blogDetails(blog)}
+    </div>
+  )
+}
 
 export default Blog
