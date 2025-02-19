67706fe253431cec780badbb740c82df4619e9c8
amylovesong <amylovesong.sun@gmail.com>
2025-02-19 21:26:00 +0800

    Add part6 exercise-6.12

---
+++ part1/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: '',
+  reducers: {
+    newNotification(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export const { newNotification } = notificationSlice.actions
+
+export default notificationSlice.reducer
