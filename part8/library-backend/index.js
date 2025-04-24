fe1e47d347f5bd767a50f0423149bac252bdcea7
amylovesong <amylovesong.sun@gmail.com>
2025-04-24 19:06:43 +0800

    Add part8 exercise-8.5

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -110,7 +110,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -120,11 +120,22 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books
       }
 
-      return books.filter(b => b.author === args.author)
+      if (args.genre && args.author) {
+        return books.filter(b => b.author === args.author)
+          .filter(b => b.genres.includes(args.genre))
+      }
+
+      if (args.author) {
+        return books.filter(b => b.author === args.author)
+      }
+
+      if (args.genre) {
+        return books.filter(b => b.genres.includes(args.genre))
+      }
     },
     allAuthors: () => authors.map(author => {
       const bookCount = books.filter(b => b.author === author.name).length
