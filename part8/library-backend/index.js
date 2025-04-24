4cf6d9b78b8fe0c077e43b3689c06eb3a4d60fc9
amylovesong <amylovesong.sun@gmail.com>
2025-04-24 19:31:21 +0800

    Add part8 exercise-8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -124,6 +124,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -175,6 +179,19 @@ const resolvers = {
       }
 
       return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      
+      const updatedAuthor = {
+        ...author,
+        born: args.setBornTo
+      }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
