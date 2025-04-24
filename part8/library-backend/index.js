670a945fc34f27d3d229ade08006ba9473aeebef
amylovesong <amylovesong.sun@gmail.com>
2025-04-24 19:22:15 +0800

    Add part8 exercise-8.6

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -104,6 +105,8 @@ const typeDefs = `
 
   type Author {
     name: String!
+    id: ID!
+    born: Int
     bookCount: Int!
   }
 
@@ -112,6 +115,15 @@ const typeDefs = `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
   }
 `
 
@@ -144,6 +156,26 @@ const resolvers = {
         bookCount
       }
     })
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const newBook = {
+        ...args,
+        id: uuid(),
+      }
+      books = books.concat(newBook)
+
+      // save new author
+      if (!authors.map(a => a.name)
+        .includes(args.author)) {
+        authors = authors.concat({
+          name: args.author,
+          id: uuid(),
+        })
+      }
+
+      return newBook
+    }
   }
 }
 
