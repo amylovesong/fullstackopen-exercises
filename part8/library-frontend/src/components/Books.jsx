8e9a47680245cef95d727f68e4454b04b4256259
amylovesong <amylovesong.sun@gmail.com>
2025-05-10 21:40:30 +0800

    Add part8 exercise-8.19

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,19 +1,38 @@
 import { useQuery } from "@apollo/client"
 import { ALL_BOOKS } from "../queries"
+import { useState } from "react"
 
 const Books = () => {
   const result = useQuery(ALL_BOOKS)
+  const [genre, setGenre] = useState('')
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
-  const books = result.data ? result.data.allBooks : []
+  const allBooks = result.data ? result.data.allBooks : []
+  console.log('allBooks:', allBooks)
+
+  const allGenres = Array.from(
+    new Set(
+      allBooks.reduce((pre, cur) => {
+        return pre.concat(cur.genres)
+      }, [])
+    )
+  )
+  console.log('allGenres:', allGenres, 'genre:', genre)
+
+  const books = genre
+    ? allBooks.filter(book => book.genres.includes(genre))
+    : allBooks
+  console.log('books:', books)
 
   return (
     <div>
       <h2>books</h2>
-
+      {
+        genre ? <div>in genre <b>{genre}</b></div> : null
+      }
       <table>
         <tbody>
           <tr>
@@ -30,6 +49,10 @@ const Books = () => {
           ))}
         </tbody>
       </table>
+      {allGenres.map((genre) => (
+        <button key={genre} onClick={() => setGenre(genre)}>{genre}</button>
+      ))}
+      <button onClick={() => setGenre(null)}>all genres</button>
     </div>
   )
 }
